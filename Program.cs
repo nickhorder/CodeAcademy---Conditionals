93c20db225a16d75b10dadad4bbc150351f4afd6
nickhorder <thunderstruck1986@hotmail.co.uk>
2021-06-08 07:17:45 +0100

    Practicing in C# with IF, IF/ELSE, ELSE/IF

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace CodeAcademy___Conditionals
             {
 // If socks is equal to or greater than 3, the code block will execute.
                 Console.WriteLine("Time to do laundry!");
+
 //TASK2 - IF/ELSE
 // Set the safe limit of people that can be in SaladMart at any one time,
 // based on Social Distancing Rules.
@@ -31,26 +32,87 @@ namespace CodeAcademy___Conditionals
                 }
 // Apply the conditional 
                 if (SaladMartNum <= SaladMartLimit)
-{
+                {
 // this code block will execute only if the amount of people at SaladMart is less than
 // or equal to the limit set in SaladMartLimit.
 
-                 Console.WriteLine($"I have made the decision for you. Because there" +
-                 $" are {SaladMartNum} people currently in SaladMart, this is equal to or less" +
-                 $" than the limit of people that can be in the restaurant, which is currently" +
-                 $" {SaladMartLimit}. It is safe to attend within Covid19 Social Distancing" +
-                 $" Rules. Go and eat there.");
-                 }
-                 else
-                 {
+                    Console.WriteLine($"I have made the decision for you. Because there" +
+                    $" are {SaladMartNum} people currently in SaladMart, this is equal to or less" +
+                    $" than the limit of people that can be in the restaurant, which is currently" +
+                    $" {SaladMartLimit}. It is safe to attend within Covid19 Social Distancing" +
+                    $" Rules. Go and eat there. \n");
+                }
+                else
+                {
 // this code block will execute if the amount of people in SaladMart exceeds the limit set
 // in SaladMartLimit.
-                 Console.WriteLine($"Because there are {SaladMartNum} people already in " +
-                     $"SaladMart, this exceeds the limit of {SaladMartLimit}, which is " +
-                     $"the limit under current Covid19 Social Distancing Rules." +
-                     $" Could you go to Rita's Jazz Cafe for your lunch instead?");
-                  }
-              
+                    Console.WriteLine($"Because there are {SaladMartNum} people already in " +
+                        $"SaladMart, this exceeds the limit of {SaladMartLimit}, which is " +
+                        $"the limit under current Covid19 Social Distancing Rules." +
+                        $" Could you go to Rita's Jazz Cafe for your lunch instead? \n");
+                }
+
+//TASK3 - ELSE/IF
+// Ask the user to confirm how many guests are coming for Games Night 2021
+                Console.WriteLine("How many people are attending the games night " +
+                    "you are hosting tonight?");
+                string gamers = Console.ReadLine();
+// Attempt conversion from string input to integer
+                if (Int32.TryParse(gamers, out int guests))
+                {
+                Console.WriteLine($"So you have {guests} coming tonight.");
+                }
+                else
+                {
+                Console.WriteLine($"'{gamers}' is not a number?! Try again.");
+               }
+//Decide on the game to be played, which will depend on how many people turn up
+                if (guests > 4)
+                {
+                    Console.WriteLine($"As you live in a tiny bedsit, {guests} " +
+                        "people is too many people to fit in. I think it should be " +
+                        "about 4 or less. Maybe tell a few of them you hate them, " +
+                        "to get the numbers down? Come back here when the numbers " +
+                        "are lower.");
+                }
+                else if (guests >= 4)
+                {
+                    Console.WriteLine($"As the number of guests is {guests} " +
+                        "we will play Monopoly and hate each other within " +
+                        "30 minutes.");
+                }
+                else if (guests >= 3)
+                {
+                    Console.WriteLine($"As the number of guests is {guests} " +
+                        "we will play Uno, which quite frankly, is fun for all the " +
+                        "family.");
+                }
+                else if (guests >= 2)
+                {
+                    Console.WriteLine($"As the number of guests is {guests} " +
+                        "we will play King in the Corner.");
+                }
+                else if (guests == 1)
+                {
+                    Console.WriteLine($"As you only have {guests} person coming tonight, " +
+                        "We are going to play Strip Poker. Close the blinds now.");
+                }
+                else if (guests == 0)
+                {
+                    Console.WriteLine($"No one is turning up? You're better than these " +
+                        $"people, find some new friends that respect your time and social " +
+                        $"efforts. Spend the evening on the PS4 or perhaps start that box " +
+                        $"set of Judge John Deed you've been meaning to watch.");
+                }
+                /*          Commenting this out for now, as else is present in the exception
+                *          handling, and this gets written out when an exception is found on 
+                *          user's input (not int). 
+                *               else
+                                {
+                                  Console.WriteLine($"You're on your own today, so play the " +
+                                        $"lonely man's game; Solitaire.");
+                                }
+                */
             }
         }
     }
